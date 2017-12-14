c285a266c8079486161a9948b77bf83fc96599db
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-12-14 11:43:00 -0800

    adds initial model and view js files/folders

---
+++ models/image.js
@@ -0,0 +1,17 @@
+'use strict';
+var app = app || {};
+
+// const API_URL = 'heroku route';
+const API_URL = 'http://localhost:3000';
+
+(function (module) {
+    function Image (obj) {
+        this.id = obj.id;
+        this.rover = obj.rover;
+        this.camera = obj.camera;
+        this.url = obj.url;
+}
+    Image.all = [];
+
+    
+})
