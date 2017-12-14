ef4e57f9496ffa28a40ae7916bbba9d4f579d473
Lizabeth Petersen <lizpetersen928@gmail.com>
2017-12-14 13:28:19 -0800

    starts building models; adds routes.js (empty)

--- models/image.js
+++ models/image.js
@@ -2,7 +2,7 @@
 var app = app || {};
 
 // const API_URL = 'heroku route';
-const API_URL = 'http://localhost:3000';
+const API_URL = 'http://localhost:3000/api/v1';
 
 (function (module) {
     function Image (obj) {
@@ -13,5 +13,20 @@ const API_URL = 'http://localhost:3000';
 }
     Image.all = [];
 
-    
-})
+    Image.fetchImages = (cb) => {
+        $.get(`${API_URL}/nasa`)
+        .then(Image.loadAll);
+        //.then(cb)
+        //.fail(console.error);
+    }
+
+    Image.loadAll = (data) => {
+        Image.all = data.map(obj => new Image(obj));
+        console.log(Image.all);
+    }
+
+    Image.prototype.toHtml = function () {
+        $('#gallery').append();
+    }
+    module.Image = Image;
+})(app);
